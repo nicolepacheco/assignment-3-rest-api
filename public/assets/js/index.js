b897483cb44220acbaf411f09ffae59ec448d225
Nicole Pachêco <nicolepacheco42@gmail.com>
2024-10-13 20:00:14 -0300

    Adds media container and render it depending on "media_type" from API

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -8,13 +8,30 @@ async function main() {
     const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${today}`)
         .then(response => response.json())
     console.log('respones from NASA API:', response)
-    const { url, title, explanation } = response
+    const { url, title, explanation, media_type } = response
     const titleContainer = document.getElementById("title-container");
     const descriptionContainer = document.getElementById("description-container");
     const pictureButton = document.getElementById("check-picture-button");
+    const mediaContainer = document.getElementById('media-container');
     titleContainer.textContent = title
     descriptionContainer.textContent = explanation
     pictureButton.href = url
+
+    if (media_type === 'image') {
+        mediaContainer.innerHTML = `<img src="${url}" alt="${explanation}" />`;
+    } else if (media_type === 'video') {
+        mediaContainer.innerHTML = `
+            <iframe
+                width="560"
+                height="315"
+                src="${url}"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen>
+            </iframe>
+        `;
+    }
+
 }
 
 main()
