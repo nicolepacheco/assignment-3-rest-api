18377eb3b80d76d7408fd100e8125c1a446ff898
Nicole Pachêco <nicolepacheco42@gmail.com>
2024-10-13 19:32:52 -0300

    Getting url, title and description and displaying it in the page

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,13 +1,20 @@
 // API Key goes here - DON'T SEND IT TO GITHUB
 const API_KEY = 'api_key'
 
-function main() {
+async function main() {
     console.log('main function')
     const today = new Date().toISOString().slice(0, 10);
     console.log('Today is: ', today)
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${today}`)
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${today}`)
         .then(response => response.json())
-        .then(data => console.log(data));
+    console.log('respones from NASA API:', response)
+    const { url, title, explanation } = response
+    const titleContainer = document.getElementById("title-container");
+    const descriptionContainer = document.getElementById("description-container");
+    const pictureButton = document.getElementById("check-picture-button");
+    titleContainer.textContent = title
+    descriptionContainer.textContent = explanation
+    pictureButton.href = url
 }
 
 main()
