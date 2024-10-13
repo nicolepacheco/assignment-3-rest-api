31017705b78f41e3f269d0f3a6bd6f6a0e3e0e65
Nicole Pachêco <nicolepacheco42@gmail.com>
2024-10-13 20:27:11 -0300

    Adds choose date form, refreshing the image/video request

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,11 +1,19 @@
 // API Key goes here - DON'T SEND IT TO GITHUB
 const API_KEY = 'api_key'
 
+const today = new Date().toISOString().slice(0, 10);
+let chooseDate = today
+
+function handleDateChange(e) {
+    console.log(e.target.value)
+    chooseDate = e.target.value
+    main()
+}
+
 async function main() {
     console.log('main function')
-    const today = new Date().toISOString().slice(0, 10);
     console.log('Today is: ', today)
-    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${today}`)
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${chooseDate}`)
         .then(response => response.json())
     console.log('respones from NASA API:', response)
     const { url, title, explanation, media_type } = response
@@ -13,7 +21,7 @@ async function main() {
     const descriptionContainer = document.getElementById("description-container");
     const pictureButton = document.getElementById("check-picture-button");
     const mediaContainer = document.getElementById('media-container');
-    titleContainer.textContent = title
+    titleContainer.textContent = `${today} - ${title}`
     descriptionContainer.textContent = explanation
     pictureButton.href = url
 
@@ -32,6 +40,18 @@ async function main() {
         `;
     }
 
+    const chooseDateContainer = document.getElementById('choose-date-container');
+    chooseDateContainer.innerHTML = `
+        <label class="lead" for="pic-date">Choose a date:</label>
+        <input
+            type="date"
+            id="pic-date"
+            name="pic-date"
+            value="${chooseDate}"
+            min="1995-06-16"
+            max="${today}"
+            onchange="handleDateChange(event)" />
+    `;
 }
 
 main()
