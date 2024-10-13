7ebfa9b1280301877bf389daba05f637a2ed90d1
Nicole Pachêco <nicolepacheco42@gmail.com>
2024-10-13 19:07:21 -0300

    Passing date as "today" in the YYYY-MM-DD format

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,7 +3,9 @@ const API_KEY = 'api_key'
 
 function main() {
     console.log('main function')
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=2014-10-01`)
+    const today = new Date().toISOString().slice(0, 10);
+    console.log('Today is: ', today)
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${today}`)
         .then(response => response.json())
         .then(data => console.log(data));
 }
