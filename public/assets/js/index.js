745738ba68b7626d7a6bb5f6a5cddebef245ee1d
Nicole Pachêco <nicolepacheco42@gmail.com>
2024-10-13 18:58:54 -0300

    Adds "date" URL Search Param

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,7 +3,7 @@ const API_KEY = 'api_key'
 
 function main() {
     console.log('main function')
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=5`)
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=2014-10-01`)
         .then(response => response.json())
         .then(data => console.log(data));
 }
